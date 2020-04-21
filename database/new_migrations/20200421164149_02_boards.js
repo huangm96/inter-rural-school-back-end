8223a23baea9802e69a0c47c8f3653f671610bb0
Min-Huang <minh.huang96@gmail.com>
2020-04-21 16:58:20 -0700

    new migration

---
+++ database/new_migrations/20200421164149_02_boards.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("boards", (boards) => {
+    boards.increments();
+
+    boards
+      .integer("user_id")
+      .unsigned()
+      .nullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("boards");
+};
